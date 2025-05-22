a08f634a67bdc619aa9566301ba48b306b65fd2f
Arijit Saha <arijitsaha102002@gmail.com>
2025-05-22 13:06:02 +0530

    project titles added to json and basic modal added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,86 @@
 import project_data from "../data/projects.json";
 
-function ProjectItem({ project }) {
+function ProjectModal({ project, index }) {
     return (
         <div
-            className="card"
-            style={{
-                width: "24rem",
-                height: "44rem",
-                boxShadow: "0 0 20px gray",
-            }}
+            className="modal fade"
+            id={index + "exampleModal"}
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
         >
-            <img src={process.env.PUBLIC_URL + project.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="text-success text-center card-title">
-                    {project.technologies.join(", ")}
-                </h5>
-                <p
-                    className="card-text p-3 text-secondary"
-                    style={{ fontSize: "1.1rem" }}
-                >
-                    {project.description}
-                </p>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1
+                            className="modal-title fs-5"
+                            id={index + "exampleModalLabel"}
+                        >
+                            {project.title}
+                        </h1>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        >
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" className="btn btn-primary">
+                            Save changes
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
+    );
+}
+
+function ProjectItem({ project, index }) {
+    console.log(index);
+    return (
+        <>
+            <div
+                className="card"
+                style={{
+                    width: "24rem",
+                    height: "44rem",
+                    boxShadow: "0 0 20px gray",
+                }}
+                role="button"
+                data-bs-toggle="modal"
+                data-bs-target={"#" + index + "exampleModal"}
+            >
+                <img
+                    src={process.env.PUBLIC_URL + project.image}
+                    className="card-img-top"
+                    alt="..."
+                />
+                <div className="card-body">
+                    <h5 className="text-success text-center card-title">
+                        {project.technologies.join(", ")}
+                    </h5>
+                    <p
+                        className="card-text p-3 text-secondary"
+                        style={{ fontSize: "1.1rem" }}
+                    >
+                        {project.description}
+                    </p>
+                </div>
+            </div>
+            <ProjectModal project={project} index={index} />
+        </>
     );
 }
 
@@ -30,14 +88,10 @@ export default function Projects() {
     return (
         <div>
             <div className="container">
-                <h2
-                    className="text-center text-dark mt-5"
-                >
+                <h2 className="text-center text-dark mt-5">
                     My Works
                 </h2>
-                <h4
-                    className="text-center text-dark"
-                >
+                <h4 className="text-center text-dark">
                     A collection of projects I've worked on.
                 </h4>
             </div>
@@ -46,6 +100,7 @@ export default function Projects() {
                     <ProjectItem
                         key={index}
                         project={project}
+                        index={index}
                     />
                 ))}
             </div>
